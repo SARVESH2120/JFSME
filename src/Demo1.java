898acd36084692baff88f13843d25dd5d1e1ccb1
SARVESH2120 <sarveshguptagkp18@gmail.com>
2026-01-04 21:30:26 +0530

    add a demo2 file

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,5 +1,15 @@
+
 public class Demo1 {
+    void disp(){
+        System.out.println("disp is called by instance");
+    }
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        Demo1 demo1 = new Demo1();
+        System.out.println("hello");
+        demo1.disp();
+
+        String name  = Thread.currentThread().getName();
+        System.out.println("Name of current thread is " + name);
     }
 }
